0fb3661a2ea61ff8a85d129e409eb3d440113e7a
napev-PS <napev-PS@192.168.0.20>
2019-12-23 00:27:14 -0500

    new

---
+++ JavaCore/src/Application.java
@@ -0,0 +1,20 @@
+
+public class Application extends Employee{
+
+	double bonus = 40000d;
+	
+
+	public static void main(String[] args) {
+
+		
+		
+		Application a1 = new Application ();
+		
+		System.out.println("Employees salary is :" + a1.salary + "and bonus is :" +a1.bonus);
+		
+	}
+
+
+	
+	}
+
